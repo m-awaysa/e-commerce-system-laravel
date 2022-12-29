ccce438f220a7fec6ddbad3c8e322de800e67b02
Mohammed awaysa <83041509+m-awaysa@users.noreply.github.com>
2022-12-29 09:17:53 +0300

    Update HomeImageSeeder.php

--- database/seeders/HomeImageSeeder.php
+++ database/seeders/HomeImageSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\HomeIMage;
+use App\Models\HomeImage;
 
 class HomeImageSeeder extends Seeder
 {
