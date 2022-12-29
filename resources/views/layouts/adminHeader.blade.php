081d9ee4b4fbc3685ece2ac9112d49e242df3620
theawaysa <theawaysa@gmail.com>
2022-12-29 10:50:38 +0200

    nice

--- resources/views/layouts/adminHeader.blade.php
+++ resources/views/layouts/adminHeader.blade.php
@@ -10,8 +10,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>E-Commerce</title>
+    <link rel="shortcut icon" href="{{asset('images/title-image.png')}}">
     <!-- bootstrap -->
-    <!-- <link href="{{asset('css/all.min.css')}}" rel="stylesheet" type="text/css"> -->
+
     <!-- Custom fonts for this template-->
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
--- resources/views/layouts/publicHeader.blade.php
+++ resources/views/layouts/publicHeader.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Commerce</title>
+    <link rel="shortcut icon" href="{{asset('images/title-image.png')}}">
     <script src="https://kit.fontawesome.com/25a6d913fa.js" crossorigin="anonymous"></script>
-
     <!-- Box Icons  -->
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <!-- Styles  -->
